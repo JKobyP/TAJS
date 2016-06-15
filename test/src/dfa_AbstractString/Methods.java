a2c6602adab6f8da01846ffe63ce7fb7357b12bb
reu-9 <reu-9@secs.oakland.edu>
2016-06-15 09:57:23 -0400

    More tests added

--- test/src/dfa_AbstractString/Methods.java
+++ test/src/dfa_AbstractString/Methods.java
@@ -15,6 +15,18 @@ import static org.junit.Assert.assertEquals;
 public class Methods {
 
     @Test
+    public void AbstractString_string_test() {
+        AbstractString accept_ab = new AbstractString("ab");
+
+        assertEquals(true, accept_ab.run("ab"));
+        assertEquals(false, accept_ab.run("ba"));
+        assertEquals(false, accept_ab.run("aab"));
+        assertEquals(false, accept_ab.run("abb"));
+        assertEquals(false, accept_ab.run("abab"));
+        assertEquals(false, accept_ab.run("aaa"));
+    }
+
+    @Test
     public void isEmpty_test() {
         AbstractString empty = AbstractString.newEmptyAbstractString();
         AbstractString full = AbstractString.anyString();
@@ -26,36 +38,51 @@ public class Methods {
     @Test
     public void uIntString_test() {
         AbstractString uInt = new AbstractString("").uIntString();
-        Automaton ui_dfa = uInt.getDfa();
 
-        assertEquals(true, BasicOperations.run(ui_dfa,"1234"));
-        assertEquals(true, BasicOperations.run(ui_dfa,"4294967294"));
-        assertEquals(false, BasicOperations.run(ui_dfa,"4294967295"));
-        assertEquals(false,BasicOperations.run(ui_dfa,"-14635677"));
-        assertEquals(true, BasicOperations.run(ui_dfa,"0000000"));
-        assertEquals(false, BasicOperations.run(ui_dfa,"run!"));
+        assertEquals(true, uInt.run("1234"));
+        assertEquals(true, uInt.run("4294967294"));
+        assertEquals(false, uInt.run("4294967295"));
+        assertEquals(false, uInt.run("-14635677"));
+        assertEquals(true, uInt.run("0000000"));
+        assertEquals(false, uInt.run("4.5"));
+        assertEquals(false, uInt.run("run!"));
     }
 
     @Test
     public void otherNumStr_test() {
         AbstractString oNum = new AbstractString("").otherNumString();
-        Automaton oNum_dfa = oNum.getDfa();
 
-        assertEquals(false, BasicOperations.run(oNum_dfa,"1234"));
-        assertEquals(true,BasicOperations.run(oNum_dfa,"-14635677"));
-        assertEquals(false, BasicOperations.run(oNum_dfa,"0000000"));
-        assertEquals(false, BasicOperations.run(oNum_dfa,"run!"));
+        assertEquals(false, oNum.run("1234"));
+        assertEquals(true, oNum.run("-14635677"));
+        assertEquals(false, oNum.run("0000000"));
+        assertEquals(true, oNum.run("4.6"));
+        assertEquals(true, oNum.run("-4.6"));
+        assertEquals(false, oNum.run("run!"));
     }
 
     @Test
     public void getAnyNumberString_test() {
         AbstractString nStr = new AbstractString("").getAnyNumberString();
-        Automaton nStr_dfa = nStr.getDfa();
 
-        assertEquals(true, BasicOperations.run(nStr_dfa,"1234"));
-        assertEquals(true,BasicOperations.run(nStr_dfa,"-14635677"));
-        assertEquals(false, BasicOperations.run(nStr_dfa,"-run"));
-        assertEquals(false, BasicOperations.run(nStr_dfa,"run!"));
+        assertEquals(true, nStr.run("1234"));
+        assertEquals(true, nStr.run("12.34"));
+        assertEquals(true, nStr.run("12.34e7"));
+        assertEquals(true, nStr.run("12.34e-7"));
+        assertEquals(true, nStr.run("12.34e+7"));
+        assertEquals(true, nStr.run("-14635677"));
+        assertEquals(false, nStr.run("-run"));
+        assertEquals(false, nStr.run("run!"));
+        assertEquals(true, nStr.run("-Infinity"));
+        assertEquals(true, nStr.run("Infinity"));
+        assertEquals(true, nStr.run("NaN"));
+    }
+
+    @Test
+    public void isLessThan_test() {
+        AbstractString a = new AbstractString("kappa");
+        AbstractString b = new AbstractString("kappag");
+
+        assertEquals(false, a.isLessThan(b));
     }
 
     public static void main(String[] args) {
